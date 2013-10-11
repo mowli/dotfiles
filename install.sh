e5312c8b6b81d5ffde9e5e64cc0995df31e0f783
Oliver Noack <oliver.noack@zweitag.de>
2013-10-11 12:24:11 +0200

    Ignore git directory in install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ dir=~/dotfiles
 cd $dir
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-for file in `find . -type f ! -name "install.sh" -exec basename {} \;`; do
+for file in `find . -type f ! -name "install.sh" ! -path "*/.*/*" -exec basename {} \;`; do
   echo $file
   mv ~/."$file" ~/."$file"_`date +%s`_bak
   echo "Creating symlink to $file in home directory."
